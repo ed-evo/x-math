9c9ee1bce4c3621240d73b9b64a64b5dad461afb
Lingyong Sun <lysz210@gmail.com>
2022-01-22 11:02:15 +0100

    full streamMode + start&stop

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         {{ history.length }}
-        <v-btn @click="interactions.next()">Start</v-btn>
+        <v-btn @click="control$.next(true)">Start</v-btn>
+        <v-btn @click="control$.next(false)">stop</v-btn>
       </v-card>
       <v-card v-if="question">
         <v-card-title class="headline text-center d-block" >
@@ -15,7 +16,7 @@
             <v-btn
               text
               v-for="choice in question.choices" :key="choice"
-              @click="change(choice, question)"
+              @click="interactions$.next({choice, question})"
             >
               {{ isNaN(choice) ? 'nessuno' : choice }}
             </v-btn>
@@ -43,10 +44,15 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { switchMapTo, Subject, tap, timer, map } from 'rxjs'
-import { random, shuffle, sample } from "lodash"
+import { switchMap, Subject, tap, timer, map, filter, merge, EMPTY } from 'rxjs'
+import { random, shuffle, sample, isNil } from "lodash"
 import { Multiplication } from '~~/models/expressions';
 import { MultiChoice } from '~~/models/questions';
+
+interface AnswarEvent {
+  choice: number;
+  question: Multiplication;
+}
 
 const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const tabelline = NUMBERS.flatMap(a => NUMBERS.map(b => new Multiplication(a, b)));
@@ -62,40 +68,42 @@ export default Vue.extend({
   },
   data () {
     return {
-      interactions: new Subject(),
+      control$: new Subject(),
+      interactions$: new Subject(),
       history: []
     }
   },
   created () {
     let timer$ = timer(0, 3000);
-    this.interactions.pipe(
+    merge(
+      this.control$.pipe(map(start => start === true)),
+      this.interactions$.pipe(map((value) => !isNil(value)))
+    ).pipe(
       tap(console.log),
-      switchMapTo(timer$),
+      switchMap(goOn => goOn ? timer$ : EMPTY),
       tap(console.log.bind(console, "main stream")),
-      map(this.createQuesiton),
+      map(() => sample(tabelline)),
+      map((next: Multiplication) => {
+        let choices = new Set<number>();
+        if (random(1, 100) % 4 < 3) {
+          choices.add(next.value);
+        }
+        do {
+          let rnd = sample(tabelline);
+          choices.add(rnd.value);
+        } while (choices.size < 3);
+
+        choices = new Set<number>(shuffle(Array.from(choices)));
+        choices.add(NaN);
+        return new MultiChoice(next, choices);
+      }),
       tap(console.log)
     ).subscribe(q => this.history.unshift(q));
-  },
-  methods: {
-    createQuesiton () {
-      let next = sample(tabelline);
-      let choices = new Set<number>();
-      if (random(1, 100) % 4 < 3) {
-        choices.add(next.value);
-      }
-      do {
-        let rnd = sample(tabelline);
-        choices.add(rnd.value);
-      } while (choices.size < 3);
-
-      choices = new Set<number>(shuffle(Array.from(choices)));
-      choices.add(NaN);
-      return new MultiChoice(next, choices);
-    },
-    change (value, question) {
-      question.answer = value
-      this.interactions.next()
-    }
+    this.interactions$
+      .pipe(
+        filter((value) => !isNil(value)),
+        tap(console.log.bind(console, "answering"))
+      ).subscribe((value) => value.question.answer = value.choice)
   }
 })
 </script>
